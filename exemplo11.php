f4a4cefb46a042e53162398456d2966877f45dcc
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-06-27 11:59:37 -0300

    Update exemplo11.php

--- exemplo11.php
+++ exemplo11.php
@@ -7,7 +7,6 @@
     <title>Operadores</title>
 </head>
 <body>
-    <style></style>
 <h1>Operadores</h1> 
 <h2>Tipos de Operadores</h2>
 <ul>
@@ -18,5 +17,28 @@
     <li>Operadores lógicos</li>
     <li>Operador ternário</li>
 </ul>
+
+<?php
+// Pré e Pós-incremento
+$a = 1;
+$b = 3;
+$c = 5;
+$res1 = ++$b - $a;
+$res2 = $c-- + $a;
+$res3 = --$a + $c++;
+echo "a = $a<br> b = $b<br> c = $c<br><br>";
+echo "res1 = $res1<br> 
+      res2 = $res2<br>
+      res3 = $res3<br>";
+echo "<hr>";
+?>
+
+<?php
+//Operadores Binários
+$num = 14;
+$deslocado = $num >> 1;  // >> desloca 1 bit para direita
+echo $deslocado;
+?>
+
 </body>
 </html>
