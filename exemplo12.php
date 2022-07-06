3e69ca7d325bc70d1f4ee15cb0a839bb36288d99
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-07-06 19:05:02 -0300

    Update exemplo12.php

--- exemplo12.php
+++ exemplo12.php
@@ -7,8 +7,18 @@
     <title>Estruturas de controle</title>
 </head>
 <body>
+    <style> p{font-size: 30px}</style>
    <h1>Comandos Condicionais</h1>
 
+<h2>If</h2>
+<p>
+    if (exp1) {  <br>
+        bloco1 <br>
+    } elseif (exp2) { <br>
+        bloco2 <br>
+    } else <br>
+    {bloco3}
+</p>
 <?php
 $prova1 = 7;
 $prova2 = 5;
@@ -27,17 +37,32 @@ elseif ($nota < 9)
 else 
     $desempenho = "Excelente";
 
-echo "O seu desempenho foi $desempenho <hr>";
+echo "O seu desempenho foi $desempenho <br>";
 ?>
 
 <?php 
-$nota = 0;
-if ($nota == 10)
-{
-echo "Parabéns <br>";
+$nota = 10;
+if($nota == 10) {
+    echo "Parabéns <br>";
     echo "Você tirou a nota máxima";
-} else { echo "Você não tirou a nota máxima";}
+} else { 
+    echo "Você não tirou a nota máxima";
+}
 ?>
 
+<p><strong>Sintaxe alternativa usando o endif</strong></p>
+<?php
+// if ():
+//     bloco1
+// elseif ():
+//     bloco2
+// else: 
+//     bloco3
+// endif;
+?>
+<hr>
+
+
+<h2>Switch</h2>
 </body>
 </html>
