14cfbb7e68dd9acf13f9af7fddd89031e746e57e
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-07-07 03:01:35 -0300

    Update exemplo12.php

--- exemplo12.php
+++ exemplo12.php
@@ -7,17 +7,19 @@
     <title>Estruturas de controle</title>
 </head>
 <body>
-    <style> p{font-size: 30px}</style>
+    <style> h2,p{font-size: 30px}</style>
    <h1>Comandos Condicionais</h1>
 
 <h2>If</h2>
+<p><strong>if(se), elseif(senão se) e else(senão)</strong></p>
 <p>
     if (exp1) {  <br>
         bloco1 <br>
     } elseif (exp2) { <br>
         bloco2 <br>
-    } else <br>
-    {bloco3}
+    } else { <br>
+    bloco3 <br>
+    }
 </p>
 <?php
 $prova1 = 7;
@@ -26,27 +28,27 @@ $nota = ($prova1 + $prova2)/2;
 
 // Não tem diferença usar COM ou SEM chaves (Mas com chaves {} é mais utilizado no mercado)
 
-if ($nota < 3)
+if ($nota < 3) {
     $desempenho = "Péssimo";
-elseif ($nota < 5)
+} elseif ($nota < 5) {
     $desempenho = "Ruim";
-elseif ($nota < 7)
+} elseif ($nota < 7) {
     $desempenho = "Médio";
-elseif ($nota < 9)
+} elseif ($nota < 9) {
     $desempenho = "Bom";
-else 
+} else {
     $desempenho = "Excelente";
-
-echo "O seu desempenho foi $desempenho <br>";
+}
+echo "<p>O seu desempenho foi $desempenho </p><br>";
 ?>
 
 <?php 
 $nota = 10;
 if($nota == 10) {
-    echo "Parabéns <br>";
-    echo "Você tirou a nota máxima";
+    echo "<p>Parabéns <br>";
+    echo "<p>Você tirou a nota máxima</p>";
 } else { 
-    echo "Você não tirou a nota máxima";
+    echo "<p>Você não tirou a nota máxima</p>";
 }
 ?>
 
@@ -64,5 +66,58 @@ if($nota == 10) {
 
 
 <h2>Switch</h2>
+<p>Utilizando <strong>If</strong> e <strong>Switch</strong> na mesma coisa</p>
+
+<p>Exemplo:</p>
+<?php
+// if
+$numero = 0;
+if ($numero == 0) {
+    echo "<p>Número vale 0 (if)</p>";
+} elseif ($numero == 1) {
+    echo "<p>Número vale 1 (if)</p>";
+} elseif ($numero == 2) {
+    echo "<p>Número vale 2 (if)</p>";
+} else {
+    echo"<p>O número não é 1, 2 e 3 (IF)</p> <br>";
+}
+
+
+// Switch
+$numero = 2;
+switch ($numero)
+{
+    case 0:
+        echo "<p>número vale 0 (switch)</p>";
+        break;
+    case 1:
+        echo "<p>número vale 1 (switch)</p>";
+        break;
+    case 2:
+        echo "<p>número vale 2 (switch)</p>";
+        break;
+     default:
+         echo "<p>O número não é 1, 2 e 3 (SWITCH)</p> <br>";
+         break;
+}
+
+?>
+
+<?php
+$opcao = "simmm";
+
+switch($opcao)
+{
+    case 's':
+        echo "<p>Você escolheu a opção Sim</p>";
+        break;
+    case 'n':
+        echo "<p>Você escolheu a opção Não (switch)</p>";
+        break;
+    default: 
+        echo "<p>Resposta inválida</p>"; 
+}
+// A opção DEFAULT tem a mesma função da opção else no comando IF, se todas as expressões anteriores retornarem falso, será executado o bloco de comando que aparece após o default, seu uso não é obrigatório no comando switch
+?>
 </body>
 </html>
