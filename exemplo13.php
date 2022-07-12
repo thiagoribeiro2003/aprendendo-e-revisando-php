ce2bac2f56d99a4c66a8d301c2b0ca2ae597f4ae
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-07-12 00:44:13 -0300

    for com array bidimensional

--- exemplo13.php
+++ exemplo13.php
@@ -72,15 +72,35 @@ echo " $num,";
 echo "<br>"
 ?>
 
+
 <?php
 // fazendo um decremento
-echo "O valor é";
+echo "Estou fazendo uma contagem regressiva";
 for ($cont = 10; $cont > 0; $cont--)
 {
     echo " $cont,";
 }
 
-echo " ...FIM";
+echo " ...FIM<br>";
+?>
+
+
+<?php
+// for de um array bidimensional
+$vetor[0][0] = "elemento00";
+$vetor[0][1] = "elemento01";
+$vetor[1][0] = "elemento10";
+$vetor[1][1] = "elemento11";
+for($i = 0; $i<2; $i++)
+{
+    for($k = 0; $k<2; $k++)
+    {
+        echo "O elemento da posição $i,$k é ";
+        echo $vetor [$i][$k]. "<br>";
+    }
+}            
+
+
 ?>
 
 </body>
