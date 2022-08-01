89aff5256d2622f3676ea1b05ac8ac69c37bc73c
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-08-01 20:46:32 -0300

    Update funcoes-e-oo-exemplo5.php

--- funcoes-e-oo-exemplo5.php
+++ funcoes-e-oo-exemplo5.php
@@ -1,4 +1,14 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Função envolvendo a data atual</h1>
+    <?php
 // Função que devolve a data atual
 function retornaData(){
     $agora = time();
@@ -30,5 +40,9 @@ function retornaData(){
     return $data_atual;  
 }
 
-$hoje = retornaData();
-echo $hoje;
+    $hoje = retornaData();
+    echo $hoje;
+    ?>
+</body>
+</html>
+
