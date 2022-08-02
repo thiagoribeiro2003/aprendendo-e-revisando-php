7397586acc4bfcdcc28d0bd623cf235c1d82bf12
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-08-02 11:44:43 -0300

    Joga

---
+++ funcoes-e-oo-exemplo6.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Função para verificar um CPF</title>
+</head>
+<body>
+<h1>Utilizando funções para verificar um CPF</h1>
+<?php 
+
+?>
+</body>
+</html>
+<?php
+function cpf_errado(){
+
+}
