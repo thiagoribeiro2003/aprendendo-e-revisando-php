21ab7213a23083803e536651eed48ab91c2841ba
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-08-02 23:30:13 -0300

    Create funcoes-e-oo-exemplo6.php

---
+++ funcoes-e-oo-exemplo6.php
@@ -0,0 +1,21 @@
+<?php
+$resultado = CPF::validar("511.119.178-09");
+var_dump($resultado);
+
+function CPF(){
+    return bool;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Função para validar CPF</title>
+</head>
+<body>
+<h1>Criando função para validar CPF</h1>
+</body>
+</html>
